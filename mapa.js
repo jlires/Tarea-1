100c662ec88fd153d5f9876d755c6fa28c410325
jlires <jlires@uc.cl>
2015-04-05 23:27:29 -0300

    commit6

--- mapa.js
+++ mapa.js
@@ -3,23 +3,35 @@ function loadMap1() {
 	window.map = L.mapbox.map('map', 'jlires.li2in2gd');
 	map.setView([0, 0], 2);
 
+	
 	var blueIcon = L.icon({
     	iconUrl: "blue-marker.png",
     	iconSize: [32, 32],
     	iconAnchor: [16, 16],
-    	popupAnchor: [0, 0]
+    	popupAnchor: [200, 100]
 		});
+
+	var customOptions =
+        {
+        'maxWidth': '500',
+        'className' : 'custom'
+        };
+
 
 	var marker = [];
 	$.getJSON("muestreo_usuarios.json", function(json) {
     	for (i = 0; i < 500; i++) {
-    		marker[i] = L.marker([json.users[i].latitude, json.users[i].longitude]).addTo(map);
-    		marker[i].setIcon(blueIcon);
+    		var ubicacion = "http://maps.googleapis.com/maps/api/streetview?size=200x100&location=" + String(json.users[i].latitude) + ',' + String(json.users[i].longitude) + "&heading=151.78&pitch=-0.76&sensor=false";
+    		var customPopup = "<br/><img src='"+ubicacion+"' alt='maptime logo gif' width='350px'/>" ;
+    		marker[i] = L.marker([json.users[i].latitude, json.users[i].longitude], {icon: blueIcon}).bindPopup(customPopup,customOptions).addTo(map);
 		}
 	});
 
 
 }
+
+
+
 
 function loadMap2() {
 	L.mapbox.accessToken = 'pk.eyJ1IjoiamxpcmVzIiwiYSI6ImtBRGF4OWMifQ.PRJWq99Jh9amaB8DesBeOA';
@@ -38,15 +50,21 @@ function form1() {
     	iconUrl: "green-marker.png",
     	iconSize: [32, 32],
     	iconAnchor: [16, 16],
-    	popupAnchor: [0, 0]
+    	popupAnchor: [200, 100]
 		});
 
 	var blueIcon = L.icon({
     	iconUrl: "blue-marker.png",
     	iconSize: [32, 32],
     	iconAnchor: [16, 16],
-    	popupAnchor: [0, 0]
+    	popupAnchor: [200, 100]
 		});
+
+	var customOptions =
+        {
+        'maxWidth': '500',
+        'className' : 'custom'
+        };
 
 	for(i=0;i<markers.length;i++) {
     	map.removeLayer(markers[i]);
@@ -55,8 +73,9 @@ function form1() {
 
 	$.getJSON("input3.json", function(json) {
     	for (i = 0; i < json.usuarios[usuario]["check-ins"].length; i++) {
-    		markers[i] = L.marker([json.usuarios[usuario]["check-ins"][i].latitude, json.usuarios[usuario]["check-ins"][i].longitude]).addTo(map);
-    		markers[i].setIcon(blueIcon)
+    		var ubicacion = "http://maps.googleapis.com/maps/api/streetview?size=200x100&location=" + String(json.usuarios[usuario]["check-ins"][i].latitude) + ',' + String(json.usuarios[usuario]["check-ins"][i].longitude) + "&heading=151.78&pitch=-0.76&sensor=false";
+    		var customPopup = "<br/><img src='"+ubicacion+"' alt='maptime logo gif' width='350px'/>" ;
+    		markers[i] = L.marker([json.usuarios[usuario]["check-ins"][i].latitude, json.usuarios[usuario]["check-ins"][i].longitude], {icon: blueIcon}).bindPopup(customPopup,customOptions).addTo(map);
 		}
 	});	
 
@@ -65,7 +84,9 @@ function form1() {
     	$.getJSON("input3.json", function(json) {
     	for (i = 0; i < json.usuarios[usuario]["amigos"].length; i++) {
     		for (j = 0; j < json.usuarios[usuario]["amigos"][i]["check-ins"].length; j++) {
-    			markers.push(L.marker([json.usuarios[usuario]["amigos"][i]["check-ins"][j].latitude, json.usuarios[usuario]["amigos"][i]["check-ins"][j].longitude]).addTo(map).setIcon(greenIcon));
+    			var ubicacion = "http://maps.googleapis.com/maps/api/streetview?size=200x100&location=" + String(json.usuarios[usuario]["amigos"][i]["check-ins"][j].latitude) + ',' + String(json.usuarios[usuario]["amigos"][i]["check-ins"][j].longitude) + "&heading=151.78&pitch=-0.76&sensor=false";
+    			var customPopup = "<br/><img src='"+ubicacion+"' alt='maptime logo gif' width='350px'/>" ;
+    			markers.push(L.marker([json.usuarios[usuario]["amigos"][i]["check-ins"][j].latitude, json.usuarios[usuario]["amigos"][i]["check-ins"][j].longitude], {icon: greenIcon}).bindPopup(customPopup,customOptions).addTo(map));
     			}
 			}
 
